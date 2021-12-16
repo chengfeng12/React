b909906cf8c0a59a4529fc057361f9ca129ff479
yxr <913457727@qq.com>
2021-12-16 17:30:06 +0800

    add: 漏了一个文件

---
+++ react_staging/src/components/Hello/hello.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './hello.css'
+class Hello extends Component {
+    render() {
+        return (
+            <div className="title">
+                我是 Hello 组件
+            </div>
+        );
+    }
+}
+
+export default Hello;
