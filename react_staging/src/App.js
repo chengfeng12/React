3c416dfe299a5ff726fe784030e4b19775c0af9a
yxr <913457727@qq.com>
2021-12-21 20:22:52 +0800

    add: 路由的基本使用，采坑之旅，当前 react 属于新版本

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,59 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Loayouts from "./components/Loayouts";
+import { BrowserRouter } from "react-router-dom";
 export default class App extends Component {
-    requestData = async () => {
-        /**
-         * 关注点分离
-         *     关注点分离是日常生活和生产中广泛使用的解决复杂问题的一种系统思维方法。大体思路是,先将复杂问题做合理的分解,再分别仔细研究问题的不同侧面(关注点)，最后综合各方面的结果，合成整体的解决方案
-         */
-
-        // 使用 fetch 请求 （未优化）
-        fetch("url").then(
-            response => {
-                console.log("连接服务器成功了", response);
-                return response.json()
-            },
-            error => {
-                console.log("连接服务器失败了", error);
-                // 如果没有 return promise 对象 则依然会进入 数据请求成功, 添加则 不会进入 console.log("请求数据失败了", error);
-                return new Promise(() => {})
-            }
-        ).then(
-            resopnse => {
-                console.log("请求数据成功了", resopnse);
-            },
-            error => {
-                console.log("请求数据失败了", error);
-            }
-        )
-
-        // 优化
-        fetch("url").then(
-            response => {
-                console.log("连接服务器成功了", response);
-                return response.json()
-            }
-        ).then(
-            response => {
-                console.log("请求数据成功了", response);
-            }
-        ).catch(error => {
-            console.log("请求失败了", error);
-        })
-
-        // 在优化
-        try {
-            let response = await fetch("url");
-            let data = await response.json();
-            console.log("请求数据成功了", data);
-        } catch (error) {
-            console.log("请求失败了", error);
-        }
-    }
-
     render() {
         return (
             <div className="App">
-                <button onClick={this.requestData}>请求数据</button>
+                <BrowserRouter>
+                    <Loayouts />
+                </BrowserRouter>
             </div>
         )
     }
---
+++ react_staging/src/components/Loayouts/index.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Header from "../Header"; // 一般组件
+import About from "../../pages/About"; // 路由组件
+import Home from "../../pages/Home";
+import { Route, Link, Routes, BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+export default class Loayouts extends Component {
+  /**
+   * 路由的基本使用
+   *     1. 明确好界面中的导航区、展示区
+   *     2. 导航区域的 a 标签改为 Link 标签
+   *            <Link className="link-item" to="/about">About</Link>
+   *     3. 展示区写 Route 标签进行路径的匹配
+   *            <Route path="/about" component={About} />
+   *            新写法 <Route path="/about" element={<div> {  <About /> } </div>} />
+   *     4. <App /> 的最外层包裹 一个 <BrowserRouter> 或者 <HashRouter> (带 # 号)
+   * 
+   * 路由组件与一般组件
+   *     1. 写法不同
+   *          一般组件 <Demo />
+   *           路由组件: <Route path="/demo" component={Demo} />
+   *     2. 存放位置不同
+   *          一般组件 components
+   *          路由组件 page
+   *     3. 接收到的 props 不同
+   *          一般组件 传什么就就收什么
+   *          路由组件 (旧版本)
+   *              history：
+   *                  go: f go(n)
+   *                  goBack: f goBack()
+   *                  goForward: f goForward()
+   *                  push: f push(path, state)
+   *                  replace: f replace(path, state)
+   *              location:
+   *                  pathname: "/about"
+   *                  search: ""
+   *                  state: undefined
+   *              match:
+   *                  params: {}
+   *                  path: "/about"
+   *                  url: "/about"
+   *          新版本 （暂不清楚）
+   *  */
+  render() {
+    return (
+      <div className="layout">
+        <Header></Header>
+          <div className="view">
+            <div className="tab-list">
+              {/* 在 React 中靠路由链接实现切换组件---编写路由组件链接 */}
+              <Link className="link-item" to="/about">About</Link>
+              <Link className="link-item" to="/home">Home</Link>
+            </div>
+            <div className="route-view">
+              <Routes>
+                {/**
+                 * 之前的版本是 component 17 版本之后改为 element ，用法上也发生了改变
+                 *     之前 component={About}
+                 *     现在 element={<div>
+                              {
+                                <About />
+                              }
+                            </div>} 需要 Routes 标签包裹
+                 */}
+
+                 {/* 注册路由 */}
+                <Route path="/about" element={<div>
+                  {
+                    <About />
+                  }
+                </div>} />
+                <Route path="/home" element={<div>
+                  {
+                    <Home />
+                  }
+                </div>} />
+              </Routes>
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
