643c6dec5d1bc080c64b083ea7df590f12116d70
YXR <913457727@qq.com>
2023-04-03 15:21:35 +0800

    fix: 更换脚本命令

--- React18/react-ts/src/App.tsx
+++ React18/react-ts/src/App.tsx
@@ -1,12 +1,15 @@
+import { ConfigProvider } from 'antd';
 import appStyle, {AppMain} from './App.style';
 import { Global } from '@emotion/react'
 import LogList from './components/LogList'
 function App() {
   return (
-    <AppMain className="App">
+    <ConfigProvider>
+      <AppMain className="App">
       <Global styles={appStyle()}></Global>
       <LogList ></LogList>
     </AppMain>
+    </ConfigProvider>
   );
 }
 
