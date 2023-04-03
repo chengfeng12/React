126aa0b8054c9e4ad3d8f407b0d13b144df6ff0e
YXR <913457727@qq.com>
2023-04-03 16:32:25 +0800

    test: eslint

--- React18/react-ts/src/App.tsx
+++ React18/react-ts/src/App.tsx
@@ -1,14 +1,14 @@
-import { ConfigProvider } from 'antd';
-import appStyle, {AppMain} from './App.style';
-import { Global } from '@emotion/react'
-import LogList from './components/LogList'
+import { ConfigProvider } from "antd";
+import appStyle, { AppMain } from "./App.style";
+import { Global } from "@emotion/react";
+import LogList from "./components/LogList";
 function App() {
   return (
     <ConfigProvider>
       <AppMain className="App">
-      <Global styles={appStyle()}></Global>
-      <LogList ></LogList>
-    </AppMain>
+        <Global styles={appStyle()}></Global>
+        <LogList></LogList>
+      </AppMain>
     </ConfigProvider>
   );
 }
--- React18/react-ts/src/components/LogList.tsx
+++ React18/react-ts/src/components/LogList.tsx
@@ -4,14 +4,17 @@ import {LogListContainer} from './LogList.style'
 
 function LogList() {
   const dateList = [{
+    id: 1,
     title: 'day 1',
     content: '第一天学习愉快',
     date: '2020-03-02'
   }, {
+    id: 2,
     title: 'day 2',
     content: '第二天学习，太简单了',
     date: '2020-03-05'
   }, {
+    id: 3,
     title: 'day 2',
     content: '第三天学习，差不多学完了',
     date: '2020-03-09'
@@ -19,7 +22,7 @@ function LogList() {
   return (
     <LogListContainer>
       {
-        dateList.map(item => <LogItem {...item}></LogItem>)
+        dateList.map(item => <LogItem {...item} key={item.id}></LogItem>)
       }
     </LogListContainer>
   );
